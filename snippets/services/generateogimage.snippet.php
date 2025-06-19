3e09f15fb967c35986645f0b7cb9e49238c0bf28
spoOkyru <spoOkyr31@gmail.com>
2025-06-20 00:26:23 +0300

    Исправлено получение значение TV-поля

--- snippets/services/generateogimage.snippet.php
+++ snippets/services/generateogimage.snippet.php
@@ -1,21 +1,36 @@
 <?php
+/**
+ * Сниппет для генерации Open Graph изображения.
+ * Используется для создания изображения, если нет заданного ogImage или imageIntro.
+ */
 $site_url = $modx->getOption('site_url');
-$og = $modx->resource->get('ogImage');
-$intro = $modx->resource->get('imageIntro');
-if ($og) {
-    return $site_url . $modx->runSnippet('pthumb', [
-        'input' => $og,
-        'options' => 'w=1200&f=webp'
-    ]);
-}
-if ($intro) {
-    return $site_url . $modx->runSnippet('pthumb', [
-        'input' => $intro,
-        'options' => 'w=1200&f=webp'
-    ]);
-}
+
 $id = $modx->resource->get('id');
 $title = $modx->resource->get('pagetitle');
 $longtitle = $modx->resource->get('longtitle');
-$token = $modx->runSnippet('ogToken');
+
+$token = $modx->runSnippet('ogToken'); // Получаем токен для проверки доступа к изображению
+
+$page = $modx->getObject('modResource', $id);
+$og = $page->getTVValue('ogImage');
+$intro = $page->getTVValue('imageIntro');
+
+echo $id . ' ' . $og . ' ' . $intro;
+
+if ($og) {
+    $pthumb = $modx->runSnippet('pthumb', [
+        'input' => $og,
+        'options' => 'w=1200&f=webp'
+    ]);
+    return $site_url . $pthumb;
+}
+
+if ($intro) {
+    $pthumb = $modx->runSnippet('pthumb', [
+        'input' => $intro,
+        'options' => 'w=1200&f=webp'
+    ]);
+    return $site_url . $pthumb;
+}
+
 return $site_url . "ogimage.php?id=$id&title=" . urlencode($title) . "&longtitle=" . urlencode($longtitle) . "&token=$token";
